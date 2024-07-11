736c8a0a4f704e7a024feef96051706d52cc2d46
bret-walda <awabkhansky@gmail.com>
2024-07-11 23:19:43 +0500

    changes some configuration of the notify plugin
    changed and added some keymaps and new default values

--- .config/nvim/after/plugin/notify.lua
+++ .config/nvim/after/plugin/notify.lua
@@ -1,3 +1,13 @@
-require('notify').setup({
+require("notify").setup({
+    stages = "slide",
     render = "compact",
+    timeout = 6000,
+
 })
+vim.notify = require("notify")
+
+local opts = {noremap = true, silent = true}
+
+vim.api.nvim_set_keymap("n", "<leader>nt", ":Telescope notify<CR>", opts)
+
+
