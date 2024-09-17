6318ad1f57e38228b1de42e3503a02755d640c17
bret-walda <awabkhansky@gmail.com>
2024-09-17 22:03:04 +0500

    Added org-mode config to vanillamacs and lazy-lock.json got updated for nvim

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,12 +9,38 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(geiser-chez paredit geiser-mit)))
+ '(org-agenda-files nil)
+ '(package-selected-packages
+   '(solarized-theme org-bullets geiser-chez paredit geiser-mit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
-(load-file "~/.emacs.d/paredit.el")
+ '(fixed-pitch ((t (:family "Iosevka Nerd Font" :height 160))))
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-info ((t (:foreground "dark orange"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "black" :font "ETBembo" :height 2.0 :underline nil))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "black" :font "ETBembo"))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))))
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
+ '(variable-pitch ((t (:family "ETBembo" :height 160 :weight thin)))))
+(load-file "~/.emacs.d/plugins/paredit.el")
+(load-file "~/.emacs.d/plugins/orgmode.el")
+(load-file "~/.emacs.d/plugins/theme.el")
 (setq geiser-chez-binary "/usr/bin/scheme")
