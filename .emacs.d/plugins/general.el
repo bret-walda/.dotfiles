64d1f221d294893e26bc17ad29590b85557150d3
bret-walda <awabkhansky@gmail.com>
2025-08-06 19:58:28 +0500

    configured sly for sbcl

--- .emacs.d/plugins/general.el
+++ .emacs.d/plugins/general.el
@@ -1,3 +1,6 @@
 ;; Makes that so the autosaves and backup files go to a specific directory
 (setq backup-directory-alist `(("." . "~/.trash/emacs-backups/"))
       auto-save-file-name-transforms `((".*" "~/.trash/emacs-autosaves/" t)))
+;;Run sly
+(require 'sly-autoloads)
+(setq inferior-lisp-program "sbcl")
